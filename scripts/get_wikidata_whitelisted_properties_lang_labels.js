080c79a0c6cfb988400ac5a08fa8def6014ee464
maxlath <k@maxlath.eu>
2020-06-15 19:21:22 +0200

    scripts: rewrite fetch_wikidata_translations using the Wikibase API instead of the query service

---
+++ scripts/get_wikidata_whitelisted_properties_lang_labels.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+const { getManyEntities } = require('wikidata-sdk')
+const { readFileSync, promises: fsPromises  } = require('fs')
+const { writeFile } = fsPromises
+const languages = readFileSync('./assets/translated_langs').toString().trim().split(' ')
+const properties = require('../original/wikidata.properties_list')
+const fetch = require('node-fetch')
+
+const urls = getManyEntities({ ids: properties, languages, props: 'labels' })
+
+const labelPerLanguage = {}
+languages.forEach(lang => labelPerLanguage[lang] = {})
+
+const getData = async () => {
+  const allProperties = {}
+  for (const url of urls) {
+    const { entities } = await fetch(url).then(res => res.json())
+    Object.assign(allProperties, entities)
+  }
+  return allProperties
+}
+
+const prepareForSave = allProperties => {
+  for (const propertyId in allProperties) {
+    const { labels } = allProperties[propertyId]
+    for (const lang in labels) {
+      labelPerLanguage[lang][propertyId] = labels[lang].value
+    }
+  }
+}
+
+const saveTranslationFiles = async () => {
+  for (const lang in labelPerLanguage) {
+    const data = labelPerLanguage[lang]
+    await writeFile(`translations/wikidata/${lang}.json`, JSON.stringify(data, null, 2) + '\n')
+  }
+}
+
+getData()
+.then(prepareForSave)
+.then(saveTranslationFiles)
+.catch(err => {
+  console.error(err)
+  process.exit(1)
+})
